2d469680dc27aac72f07fe2cc35cfb3fbb8833de
evandromottaz <evandro.motta@cantacom.com.br>
2022-05-20 09:55:41 -0300

    fix fetch

--- js/chooseIngredients.js
+++ js/chooseIngredients.js
@@ -1,6 +1,3 @@
-import { onClick, onTouchStart } from './Commons';
-import { sucessPage } from './fetch-food';
-
 export const ingredients = new Object();
 
 export function chooseIngredients(item, section) {
@@ -24,18 +21,3 @@ function saveLocalStorage(food) {
   const foodSaved = JSON.stringify(food);
   window.localStorage.setItem('order', foodSaved);
 }
-
-const sections = document.querySelectorAll('section');
-sections.forEach((section) => {
-  const items = section.querySelectorAll('.item');
-
-  items.forEach((item) => {
-    onClick(item, () => chooseIngredients(item, section));
-    onTouchStart(item, () => chooseIngredients(item, section));
-  });
-});
-
-submit.addEventListener('click', () => {
-  if (ingredients.meat && ingredients.broth)
-    sucessPage(ingredients.meat, ingredients.broth);
-});
--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import './style.css';
 import Carousel from './js/Carousel';
 import Active from './js/Active';
+import { ingredients, chooseIngredients } from './js/chooseIngredients';
+import { sucessPage } from './js/fetch-food';
+import { onClick, onTouchStart } from './js/Commons';
 
 new Active('.items-1');
 new Active('.items-2');
@@ -9,3 +12,18 @@ if (matchMedia('(max-width:770px)').matches) {
   new Carousel('.carousel-1');
   new Carousel('.carousel-2');
 }
+
+const sections = document.querySelectorAll('section');
+sections.forEach((section) => {
+  const items = section.querySelectorAll('.item');
+
+  items.forEach((item) => {
+    onClick(item, () => chooseIngredients(item, section));
+    onTouchStart(item, () => chooseIngredients(item, section));
+  });
+});
+
+submit.addEventListener('click', () => {
+  if (ingredients.meat && ingredients.broth)
+    sucessPage(ingredients.meat, ingredients.broth);
+});
