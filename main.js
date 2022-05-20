105987addc8701cf27067e12aea86345118e2459
evandromottaz <evandro.motta@cantacom.com.br>
2022-05-20 19:15:51 -0300

    fix main

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ import './style.css';
 import { Carousel } from './js/Carousel';
 import { Active } from './js/Active';
 import { ChooseIngredients } from './js/ChooseIngredients';
+import { FetchFood } from './js/FetchFood';
