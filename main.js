6a0e006237cd62f4eeecc2e33f23691439f7f45b
evandromottaz <evandro.motta@cantacom.com.br>
2022-05-20 19:25:31 -0300

    deploy...

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 import './style.css';
+import { Carousel } from './js/Carousel';
+import { Active } from './js/Active';
+import { ChooseIngredients } from './js/ChooseIngredients';
+import { FetchFood } from './js/FetchFood';
